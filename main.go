1056dc173323adfa01ba2318137258b4123a319e
techswarn <tech.swarns@gmail.com>
2024-07-24 15:47:32 +0530

    update

--- main.go
+++ main.go
@@ -8,36 +8,49 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"github.com/joho/godotenv"
 	"log"
+	"net/http"
+	"io"
 )
   
   func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-		log.Println(err)
-	}
+
+	// err := godotenv.Load()
+	// if err != nil {
+	// 	log.Fatal("Error loading .env file")
+	// 	log.Println(err)
+	// }
 	DATABASE_URL := os.Getenv("DBURL")
 	log.Println(os.Getenv("DBURL"))
 	// Use the SetServerAPIOptions() method to set the version of the Stable API on the client
-	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
-	opts := options.Client().ApplyURI(DATABASE_URL).SetServerAPIOptions(serverAPI)
-  
-	// Create a new client and connect to the server
-	client, err := mongo.Connect(context.TODO(), opts)
-	if err != nil {
-	  panic(err)
-	}
-  
-	defer func() {
-	  if err = client.Disconnect(context.TODO()); err != nil {
-		panic(err)
-	  }
-	}()
-  
-	// Send a ping to confirm a successful connection
-	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
-	  panic(err)
-	}
-	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+		opts := options.Client().ApplyURI(DATABASE_URL).SetServerAPIOptions(serverAPI)
+	  
+		// Create a new client and connect to the server
+		client, err := mongo.Connect(context.TODO(), opts)
+		if err != nil {
+		  panic(err)
+		}
+	  
+		defer func() {
+		  if err = client.Disconnect(context.TODO()); err != nil {
+			panic(err)
+		  }
+		}()
+	  
+		// Send a ping to confirm a successful connection
+		if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Err(); err != nil {
+		  panic(err)
+		}
+	
+		fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+		io.WriteString(w, "Hello from a HandleFunc #1!\n")
+	})
+
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
   }
+
+
   
